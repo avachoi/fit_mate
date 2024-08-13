f9346e51103f81cf00b29dff3b8cc8cc75d4a980
avachoi <alexbchoi@gmail.com>
2024-08-13 11:51:27 -0400

    fix-name Login Signup

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import SignUp from "../components/signup.jsx";
+import SignUp from "../components/Signup.jsx";
 import { Link } from "react-router-dom";
 
 export default function SignUpPage() {
