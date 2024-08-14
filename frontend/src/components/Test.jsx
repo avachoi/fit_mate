d4da14a78138b0e188cf181d38f72928eb433231
avachoi <alexbchoi@gmail.com>
2024-08-14 15:08:59 -0400

    fix-workoutGenerator

--- frontend/src/components/Test.jsx
+++ frontend/src/components/Test.jsx
@@ -1,3 +1,5 @@
+//just for the saving
+
 import React, { useState, useEffect } from "react";
 import {
 	getChatResponse,
@@ -6,7 +8,7 @@ import {
 } from "../utils/chatGptService";
 
 function WorkoutPlanGenerator() {
-	const [chatResponse, setChatResponse] = useState({});
+	const [chatResponse, setChatResponse] = useState("");
 	const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
 	const [error, setError] = useState(null);
 
@@ -16,20 +18,16 @@ function WorkoutPlanGenerator() {
 		// 	setChatResponse("");
 		// }
 		// else {
-		getExistingPlan()
-			.then((response) => {
-				setChatResponse(response);
-			})
-			.catch((err) => {
-				console.error("Error fetching existing plan", err);
-			});
+		getExistingPlan().then((response) => {
+			setChatResponse(response);
+		});
 		// }
 	}, []);
 
 	const handleChatSubmit = async (e) => {
 		e.preventDefault();
 		try {
-			setChatResponse({ loading: "loading" });
+			setChatResponse("loading");
 			const response = await getChatResponse("");
 			setChatResponse(response);
 
@@ -50,7 +48,15 @@ function WorkoutPlanGenerator() {
 					</button>
 				</form>
 				{error && <p style={{ color: "red" }}>{error}</p>}
-				{chatResponse.planName && (
+				{chatResponse === "loading" ? (
+					<div className="loader-container">
+						<div className="bouncing-dots">
+							<div className="dot"></div>
+							<div className="dot"></div>
+							<div className="dot"></div>
+						</div>
+					</div>
+				) : (
 					<div>
 						<h3 className="saying1">{chatResponse.planName}</h3>
 						{/* Access other properties as needed */}
@@ -59,46 +65,35 @@ function WorkoutPlanGenerator() {
 							Frequency: {chatResponse.frequencyPerWeek} times per week
 						</p>
 						{/* Render exercises */}
-						{chatResponse.exercises &&
-							chatResponse.exercises.map((exercise, index) => (
-								<div key={index}>
-									<h3 className="day">{exercise.day}</h3>
-									<table className="planTable">
-										<thead>
-											<tr className="planHead">
-												<th>Exercise</th>
-												<th>Sets</th>
-												<th>Reps</th>
-												<th>Duration</th>
-												<th>Rest</th>
-												<th>Done</th>
+						{chatResponse.exercises.map((exercise, index) => (
+							<div key={index}>
+								<h3 className="day">{exercise.day}</h3>
+								<table className="planTable">
+									<thead>
+										<tr className="planHead">
+											<th>Exercise</th>
+											<th>Sets</th>
+											<th>Reps</th>
+											<th>Duration</th>
+											<th>Rest</th>
+											<th>Done</th>
+										</tr>
+									</thead>
+									<tbody>
+										{exercise.exercisesList.map((ex, idx) => (
+											<tr key={idx}>
+												<td>{ex.name}</td>
+												<td>{ex.sets}</td>
+												<td>{ex.reps}</td>
+												<td>{ex.duration}</td>
+												<td>{ex.restTime}</td>
+												<td>{ex.done ? "V" : "X"}</td>
 											</tr>
-										</thead>
-										<tbody>
-											{exercise.exercisesList &&
-												exercise.exercisesList.map((ex, idx) => (
-													<tr key={idx}>
-														<td>{ex.name}</td>
-														<td>{ex.sets}</td>
-														<td>{ex.reps}</td>
-														<td>{ex.duration}</td>
-														<td>{ex.restTime}</td>
-														<td>{ex.done ? "V" : "X"}</td>
-													</tr>
-												))}
-										</tbody>
-									</table>
-								</div>
-							))}
-					</div>
-				)}
-				{chatResponse.loading && (
-					<div className="loader-container">
-						<div className="bouncing-dots">
-							<div className="dot"></div>
-							<div className="dot"></div>
-							<div className="dot"></div>
-						</div>
+										))}
+									</tbody>
+								</table>
+							</div>
+						))}
 					</div>
 				)}
 			</div>
