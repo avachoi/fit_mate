856b917b15adef9af93a7212c2e9198f282a9eee
avachoi <alexbchoi@gmail.com>
2024-08-14 12:22:12 -0400

    moved testing data to util

--- frontend/src/components/WorkoutPlanGenerator.jsx
+++ frontend/src/components/WorkoutPlanGenerator.jsx
@@ -1,106 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { getChatResponse, getExistingPlan } from "../utils/chatGptService";
+import {
+	getChatResponse,
+	getExistingPlan,
+	chatData,
+} from "../utils/chatGptService";
 
 function WorkoutPlanGenerator() {
 	const [chatResponse, setChatResponse] = useState("");
 	const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
 	const [error, setError] = useState(null);
-	let chatData = {
-		planName: "6-Week Weight Loss Plan",
-		goal: "Lose Weight",
-		durationWeeks: 6,
-		frequencyPerWeek: 4,
-		exercises: [
-			{
-				day: "Monday",
-				exercisesList: [
-					{
-						name: "Running",
-						sets: 1,
-						reps: null,
-						duration: "30 minutes",
-						restTime: "5 minutes",
-						description: "Moderate intensity run focusing on endurance",
-						done: false,
-					},
-				],
-			},
-			{
-				day: "Wednesday",
-				exercisesList: [
-					{
-						name: "Running",
-						sets: 1,
-						reps: null,
-						duration: "30 minutes",
-						restTime: "5 minutes",
-						description:
-							"Interval running: 2 minutes fast pace followed by 2 minutes slow pace, repeat for 30 minutes.",
-						done: false,
-					},
-					{
-						name: "Bodyweight Squats",
-						sets: 3,
-						reps: 10,
-						duration: null,
-						restTime: "1 minute",
-						description: "Focus on proper form and controlled movements.",
-						done: false,
-					},
-				],
-			},
-			{
-				day: "Friday",
-				exercisesList: [
-					{
-						name: "Running",
-						sets: 1,
-						reps: null,
-						duration: "30 minutes",
-						restTime: "5 minutes",
-						description: "Easy jog to recover while still staying active.",
-						done: false,
-					},
-					{
-						name: "Plank",
-						sets: 3,
-						reps: null,
-						duration: "30 seconds",
-						restTime: "1 minute",
-						description:
-							"Maintain a straight line from head to heels, engage core.",
-						done: false,
-					},
-				],
-			},
-			{
-				day: "Sunday",
-				exercisesList: [
-					{
-						name: "Running",
-						sets: 1,
-						reps: null,
-						duration: "30 minutes",
-						restTime: "5 minutes",
-						description:
-							"Fartlek training: alternate between sprinting and walking for improved speed and endurance.",
-						done: false,
-					},
-					{
-						name: "Lunges",
-						sets: 3,
-						reps: 10,
-						duration: null,
-						restTime: "1 minute",
-						description: "Step forward into a lunge, alternating legs.",
-						done: false,
-					},
-				],
-			},
-		],
-		notes:
-			"Ensure adequate hydration and maintain a balanced diet. Listen to your body and adjust the intensity as needed.",
-	};
+
 	useEffect(() => {
 		// Call the API to get the existing plan
 		// if (user.userPlans.length === 0) {
@@ -117,8 +26,9 @@ function WorkoutPlanGenerator() {
 		e.preventDefault();
 		try {
 			const response = await getChatResponse("");
+			setChatResponse(response);
 
-			// setChatResponse(response);
+			//for testing frontend without backend
 			setChatResponse(chatData);
 		} catch (err) {
 			console.error("Error fetching chat response:", err);
--- frontend/src/utils/chatGptService.js
+++ frontend/src/utils/chatGptService.js
@@ -49,3 +49,101 @@ export const getExistingPlan = async () => {
 		return { error: "Failed to fetch existing plans" };
 	}
 };
+
+//hard coded chat data for testing
+export const chatData = {
+	planName: "6-Week Weight Loss Plan",
+	goal: "Lose Weight",
+	durationWeeks: 6,
+	frequencyPerWeek: 4,
+	exercises: [
+		{
+			day: "Monday",
+			exercisesList: [
+				{
+					name: "Running",
+					sets: 1,
+					reps: null,
+					duration: "30 minutes",
+					restTime: "5 minutes",
+					description: "Moderate intensity run focusing on endurance",
+					done: false,
+				},
+			],
+		},
+		{
+			day: "Wednesday",
+			exercisesList: [
+				{
+					name: "Running",
+					sets: 1,
+					reps: null,
+					duration: "30 minutes",
+					restTime: "5 minutes",
+					description:
+						"Interval running: 2 minutes fast pace followed by 2 minutes slow pace, repeat for 30 minutes.",
+					done: false,
+				},
+				{
+					name: "Bodyweight Squats",
+					sets: 3,
+					reps: 10,
+					duration: null,
+					restTime: "1 minute",
+					description: "Focus on proper form and controlled movements.",
+					done: false,
+				},
+			],
+		},
+		{
+			day: "Friday",
+			exercisesList: [
+				{
+					name: "Running",
+					sets: 1,
+					reps: null,
+					duration: "30 minutes",
+					restTime: "5 minutes",
+					description: "Easy jog to recover while still staying active.",
+					done: false,
+				},
+				{
+					name: "Plank",
+					sets: 3,
+					reps: null,
+					duration: "30 seconds",
+					restTime: "1 minute",
+					description:
+						"Maintain a straight line from head to heels, engage core.",
+					done: false,
+				},
+			],
+		},
+		{
+			day: "Sunday",
+			exercisesList: [
+				{
+					name: "Running",
+					sets: 1,
+					reps: null,
+					duration: "30 minutes",
+					restTime: "5 minutes",
+					description:
+						"Fartlek training: alternate between sprinting and walking for improved speed and endurance.",
+					done: false,
+				},
+				{
+					name: "Lunges",
+					sets: 3,
+					reps: 10,
+					duration: null,
+					restTime: "1 minute",
+					description: "Step forward into a lunge, alternating legs.",
+					done: false,
+				},
+			],
+		},
+	],
+	notes:
+		"Ensure adequate hydration and maintain a balanced diet. Listen to your body and adjust the intensity as needed.",
+};
